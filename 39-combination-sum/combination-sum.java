802fe8fbff305421778c68d6346fb9c4370424dc
Eshita Yadav <113260555+Eshitacodes@users.noreply.github.com>
2024-08-08 08:35:06 -0700

    Time: 1 ms (99.92%) | Memory: 44.6 MB (31.28%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -5,7 +5,7 @@ class Solution {
         if(ind==arr.length)
         {
             if(target==0)
-                ans.add(new ArrayList<>(ds));
+                ans.add(new ArrayList<>(ds));//not a constant operation
 
             return;
         }
@@ -13,11 +13,11 @@ class Solution {
         if(arr[ind]<=target)
         {
             ds.add(arr[ind]);
-            targetSum(ind,arr,target-arr[ind],ans,ds);
+            targetSum(ind,arr,target-arr[ind],ans,ds);//pick part
             ds.remove(ds.size()-1);
         }
         
-        targetSum(ind+1,arr,target,ans,ds);
+        targetSum(ind+1,arr,target,ans,ds);//not pick that index and move to the next index
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) 
