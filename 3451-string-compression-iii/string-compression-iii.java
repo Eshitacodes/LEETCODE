263c7d83372eca492918c9998a196763dfdcc910
Eshita Yadav <113260555+Eshitacodes@users.noreply.github.com>
2024-11-05 00:17:29 +0530

    Time: 1017 ms (8.47%) | Memory: 51.6 MB (11.67%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String compressedString(String word) 
+    {
+     String comp="";
+     int arr[]=new int[26];
+     int count=1;
+     int i;
+
+     for(i=0;i<word.length()-1;i++)
+     {
+        if(word.charAt(i)==word.charAt(i+1))
+            count++;
+
+        else
+        {
+            while(count>0)
+            {
+                if(count<10)
+                    comp=comp+ count +word.charAt(i);
+            
+                else
+                {
+                    comp=comp+9+word.charAt(i);
+                }
+
+                count-=9;
+            }
+
+            count=1;
+        }
+     }
+
+     while(count>0)
+            {
+                if(count<10)
+                    comp=comp+ count +word.charAt(i);
+            
+                else
+                {
+                    comp=comp+9+word.charAt(i);
+                }
+
+                count-=9;
+            }
+
+    //  comp=comp+ count+word.charAt(i);
+
+     return comp;
+
+    }
+}
