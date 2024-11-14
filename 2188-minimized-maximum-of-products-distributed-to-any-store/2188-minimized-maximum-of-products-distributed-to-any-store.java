836f5f6ed0a51ec27673c0407b7dc4d8e6dfd394
Eshita Yadav <113260555+Eshitacodes@users.noreply.github.com>
2024-11-15 02:11:41 +0530

    Time: 22 ms (100%), Space: 56.4 MB (68.62%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) 
+    {
+        int m=quantities.length;
+       int left=1;
+       int right=Integer.MIN_VALUE;
+
+       for(int i:quantities)
+            right=Math.max(i,right);
+
+        int result=0;
+
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+
+            if(possibleToDivide(mid,quantities,n))
+                {
+                    result=mid;
+                    right=mid-1;
+                }
+
+            else
+                left=mid+1;
+        }
+
+        return result;
+
+
+    }
+
+    boolean possibleToDivide(int prodPerShop, int[] quantities,int store)
+    {
+        for(int proQuant:quantities)
+        {
+            store-=(proQuant+ prodPerShop-1)/prodPerShop;
+
+            if(store<0)
+                return false;
+        }
+        return true;
+    }
+}
