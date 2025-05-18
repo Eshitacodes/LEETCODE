4aebab36a302c6e530c52305c4d45af0addf10fa
Eshita Yadav <113260555+Eshitacodes@users.noreply.github.com>
2025-05-18 20:35:02 +0530

    Time: 0 ms (100%), Space: 42.7 MB (28.74%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -14,19 +14,22 @@ class Solution {
         if(head==null || head.next==null)
             return head;
 
-        ListNode p1=null;
-        ListNode p2=head;
-        ListNode p3=head.next;
+        ListNode p1=head;
+        ListNode p2=head.next;
+        ListNode p3=head.next.next;
+
+        p1.next=null;
 
         while(p2!=null)
         {
             p2.next=p1;
             p1=p2;
             p2=p3;
-
             if(p3!=null)
                 p3=p3.next;
         }
-        return p1;
+
+        head=p1;
+        return head;
     }
 }
